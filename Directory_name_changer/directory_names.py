d73338d116a2046b7d084907eb3c12f58371d241
Rafał Ziemianek <49795849+Rafalz13@users.noreply.github.com>
2021-06-13 16:31:06 +0200

    Initial commit
    Added Method to get directory names

--- Directory_name_changer/directory_names.py
+++ Directory_name_changer/directory_names.py
@@ -1,1 +1,15 @@
+import os
+
+def get_dir_names(path_to_dirs=""):
+    os.chdir(path_to_dirs)
+    list_dir_prep = os.listdir()
+    list_dir_post = []
+
+    for dir in list_dir_prep:
+        if "." not in dir:
+            list_dir_post.append(dir)
+    return list_dir_post
+
+
+
 
