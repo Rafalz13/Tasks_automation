eab413a654632eb9288d69f2163aa1a17b7ed17c
Rafał Ziemianek <49795849+Rafalz13@users.noreply.github.com>
2021-06-13 22:24:06 +0200

    Add functions to rename dirs
    Add change_dirs_name and go_through_dirs

--- Directory_name_changer/directory_names.py
+++ Directory_name_changer/directory_names.py
@@ -11,5 +11,23 @@ def get_dir_names(path_to_dirs=""):
     return list_dir_post
 
 
+def change_dirs_name(old="",new_name=""):
+    os.rename(old, new_name)
+
+def go_through_dirs(old_name,new_name,list_of_dirs=[]):
+    for dir in list_of_dirs:
+        os.chdir(dir)
+        change_dirs_name(old_name,new_name)
+        os.chdir("..")
+
+
+
+old_name = input("old name: ")
+new_name = input("new name: ")
+path_to_dirs = input("path to directories: ")
+list_dirs = get_dir_names(path_to_dirs)
+go_through_dirs(old_name, new_name, list_dirs)
+
+
 
 
